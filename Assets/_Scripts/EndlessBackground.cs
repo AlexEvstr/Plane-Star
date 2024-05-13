d724df6b1becd27bc6fc7ad7969db8bcc6fb19d2
Alexander Evstratov <alexdrugev@me.com>
2024-05-13 16:34:55 +0300

    Add endless bg.

---
+++ Assets/_Scripts/EndlessBackground.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EndlessBackground : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2.0f;
+    private Vector2 _zeroPosition;
+    private float _middleOfBackground;
+
+    private void Start()
+    {
+        _zeroPosition = transform.position;
+        _middleOfBackground = GetComponent<BoxCollider2D>().size.x / 2;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * _speed);
+        if (transform.position.x < (_zeroPosition.x - _middleOfBackground))
+            transform.position = _zeroPosition;
+    }
+}
